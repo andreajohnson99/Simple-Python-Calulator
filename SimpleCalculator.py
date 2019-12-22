284f5e0aaff81621a0492eeb81b8435f74a8fb11
Andrea Johnson <54482569+aztechvegan@users.noreply.github.com>
2019-12-22 15:41:39 -0500

    Create SimpleCalculator.py

---
+++ SimpleCalculator.py
@@ -0,0 +1,12 @@
+#Create two variables
+#Num1 and Num2
+#Stored two numbers inputted
+#Into variables
+#then converted them into numbers
+#Added the numbers
+#Printed a result
+
+num1 = input("Enter a number: ")
+num2 = input("Enter another number: ")
+result = float(num1) + float(num2)
+print(result)
